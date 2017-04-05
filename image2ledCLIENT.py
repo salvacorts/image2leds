2fe7df89a8c41a3aca25f631b21f1aa1bcb5fa36
salvacorts <salvacorts97@gmail.com>
2017-04-05 21:05:48 +0200

    First Commit

---
+++ image2ledCLIENT.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+### IDEA: Get palette instead of dominant colour to create a fade effect
+
+from colorthief import ColorThief
+import os, sys, socket, time
+
+# Main options
+PORT = 5000				# Port to revieve from
+SERVER = "127.0.0.1"	# Server address
+QUALITY = 5				# Quality to get dominant color
+DURATION = 15			# Time to sleep before checking again (seconds)
+
+def GetDominantColour():
+	dominantColour = -1
+
+	# Get current wallpaper
+	wallpaper = os.popen("gsettings get org.gnome.desktop.background picture-uri").read()
+	wallpaper = wallpaper.replace("file://", "").replace("'", "").replace("\n", "");
+
+	# Get dominant color from this wallpaper
+	try:
+		colours = ColorThief(wallpaper)
+		dominantColour = colours.get_color(QUALITY)
+	except:
+		print("[!] CLIENT: Error picking primary color")
+
+	return dominantColour
+
+def main():
+	lastdominantColour = "0"
+
+	# Parse arguments
+	if len(sys.argv) > 1: end = int(sys.argv[1])
+	else: end = -1
+
+	# Initialize socket configuration and connect to server
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		s.connect((SERVER, PORT))
+	except:
+		print "[!] Error connecting to server. check configuration"
+
+	i = 0;
+	while (i != end):
+		dominantColour = GetDominantColour()
+
+		if dominantColour != lastdominantColour:
+			lastdominantColour = dominantColour
+			s.sendall(str(dominantColour))
+
+		time.sleep(DURATION)
+		i += 1
+
+	s.close()
+
+
+if __name__ == '__main__':
+	main()
---
+++ image2ledSERVER.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+### IDEA: Get palette instead of dominant colour in order to create a fade effect
+
+from colorthief import ColorThief
+import pigpio as gpio
+import socket, thread
+
+# Main options
+PORT = 5000			# Port to revieve from
+RED_PIN = 22		# GPIO PIN for red
+GREEN_PIN = 23		# GPIO PIN for green
+BLUE_PIN = 24		# GPIO PIN for blue
+
+def ServeClient(clientSocket, addr):
+	# Initialize RPI GPIO configuration
+	rpi = gpio.pi()
+
+	while True:
+		# Receive dominant color
+		data = clientSocket.recv(4096)
+		if not data: break	# If no new colour is received, break
+
+		# Conver string to RGB touple of ints
+		data = data.replace("(", "").replace(")", "").replace(" ", "")
+		colours = tuple(map(int, data.split(",")))
+
+		# Check if colours are correct
+		for colour in colours:
+			if not (0 <= colour <= 255):
+				print "[!] ERROR: bad colour format"
+				pass
+
+		print colours
+
+		# Write colours to leds
+		rpi.set_PWM_dutycycle(RED_PIN, colours[0])		# Set red
+		rpi.set_PWM_dutycycle(GREEN_PIN, colours[1])	# Set green
+		rpi.set_PWM_dutycycle(BLUE_PIN, colours[2])		# Set blue
+
+def main():
+	# Initialize socket configuration
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind(('', PORT))
+	s.listen(1)
+
+	# Accept connections from clients and strat a new thread for each of them
+	while True:
+		conn, addr = s.accept()
+		print "[+] Connected by: ", addr
+		thread.start_new_thread(ServeClient, (conn, addr))
+
+	socket.close()
+
+
+if __name__ == '__main__':
+	main()
